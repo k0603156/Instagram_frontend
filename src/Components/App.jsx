02adf94402c637223a44daa85dae8ec9ee8abc80
k0603156 <k0603156@gmail.com>
2019-09-21 09:42:47 +0900

    createAccount Mutation 1

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -1,11 +1,14 @@
 import React from "react";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { useQuery } from "react-apollo-hooks";
 import styled, { ThemeProvider } from "styled-components";
 import GlobalStyles from "../Styles/GlobalStyles";
 import Theme from "../Styles/Theme";
 import AppRouter from "./Router";
 import Footer from "./Footer";
-import { useQuery } from "react-apollo-hooks";
 import { QUERY } from "./AppQuery";
+
 const Wrapper = styled.div`
   margin: 0 auto;
   max-width: 935px;
@@ -21,6 +24,7 @@ export default () => {
       <Wrapper>
         <GlobalStyles />
         <AppRouter isLoggedIn={isLoggedIn} />
+        <ToastContainer position={toast.POSITION.BOTTOM_LEFT} />
         <Footer />
       </Wrapper>
     </ThemeProvider>
---
+++ src/Routes/Auth/AuthContainer.jsx
@@ -0,0 +1,73 @@
+import React, { useState } from "react";
+import AuthPresenter from "./AuthPresenter";
+import useInput from "../../Hooks/useInput";
+import { useMutation } from "react-apollo-hooks";
+import { LOG_IN, CREATE_ACCOUNT } from "./AuthQueries";
+import { toast } from "react-toastify";
+
+export default () => {
+  const [action, setAction] = useState("logIn");
+  const username = useInput("");
+  // const password = useInput("");
+  const firstName = useInput("");
+  const lastName = useInput("");
+  const email = useInput("k0603156@gmail.com");
+  const [requestSecret] = useMutation(LOG_IN, {
+    variables: { email: email.value },
+    update: (_, { data }) => {
+      const { requestSecret } = data;
+      if (!requestSecret) {
+        toast.error("You don't have an account yet. create one");
+        setTimeout(() => {
+          setAction("signUp");
+        }, 3000);
+      }
+    }
+  });
+  const createAccount = useMutation(CREATE_ACCOUNT, {
+    variables: {
+      email: email.value,
+      username: username.value,
+      firstName: firstName.value,
+      lastName: lastName.value
+    }
+  });
+  const onSubmit = async e => {
+    e.preventDefault();
+    if (action === "logIn") {
+      if (email.value !== "") {
+        try {
+          await requestSecret();
+        } catch {
+          toast.error("Cant' request secret, try again");
+        }
+      } else {
+        toast.error("Email is required");
+      }
+    } else if (action === "signUp") {
+      if (
+        email.value !== "" &&
+        username.value !== "" &&
+        firstName.value !== "" &&
+        lastName.valu !== ""
+      ) {
+        createAccount();
+      } else {
+        toast.error("All field is required");
+      }
+    }
+  };
+
+  return (
+    <AuthPresenter
+      setAction={setAction}
+      action={action}
+      username={username}
+      // password={password}
+      firstName={firstName}
+      lastName={lastName}
+      email={email}
+      onSubmit={onSubmit}
+    />
+  );
+};
