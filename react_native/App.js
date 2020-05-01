06856a8c1649abf1efb835260198142b8b51d08e
kimyongkuk <k0603156@gmail.com>
2020-05-02 02:01:48 +0900

    Use react-native-vector-icons, react-native-splash-screen  (Android)
    @expo/vector-icons => react-native-vector-icons
    expo.AppLoading => react-native-splash-screen

--- react_native/App.js
+++ react_native/App.js
@@ -3,8 +3,8 @@
  *
  * @format
  */
-
-import React from 'react';
+'use strict';
+import React, {Component} from 'react';
 import {
   SafeAreaView,
   StyleSheet,
@@ -13,13 +13,21 @@ import {
   Text,
   StatusBar,
 } from 'react-native';
+import Icon from 'react-native-vector-icons/FontAwesome';
+import SplashScreen from 'react-native-splash-screen';
 
-const App = () => {
-  return (
-    <View>
-      <Text>App</Text>
-    </View>
-  );
-};
-
-export default App;
+export default class WelcomePage extends Component {
+  componentDidMount() {
+    SplashScreen.hide();
+  }
+  render() {
+    return (
+      <View>
+        <Text>
+          Apps
+          <Icon name="rocket" size={30} color="#900" />
+        </Text>
+      </View>
+    );
+  }
+}
